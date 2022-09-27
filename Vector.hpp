1c8bb0ed81b744aa0123afdfa4fdf24b169609cd
durantecode <durantecode@gmail.com>
2022-09-27 21:39:41 +0200

    started containers

---
+++ Vector.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 20:33:48 by ldurante          #+#    #+#             */
+/*   Updated: 2022/09/27 21:38:23 by ldurante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+#include <memory>
+#include <vector>
+
+namespace ft
+{
+	template <class T, class Alloc = std::allocator<T> >
+	class vector
+	{
+		public:
+			typedef T		value_type;
+			typedef Alloc	allocator_type;
+	};
+}
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/27 20:23:26 by ldurante          #+#    #+#             */
+/*   Updated: 2022/09/27 21:08:02 by ldurante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "List.hpp"
+#include "Vector.hpp"
+#include "Map.hpp"
+#include "Stack.hpp"
+#include "Queue.hpp"
+
+int		main(void)
+{
+	return (0);
+}
